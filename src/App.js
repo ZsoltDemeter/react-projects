8abead36b542cf50e65ef0fa834b253339324e49
ZsoltDemeter <zsoltd.com@gmail.com>
2021-05-14 12:19:55 +0300

    finished navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Navbar from "./components/navbar";
+import { BrowserRouter as Router } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Navbar />
+    </Router>
+  );
 }
 
 export default App;
