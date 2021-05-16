bc6117e8212164c26c65a52c591209d0c46433de
ZsoltDemeter <zsoltd.com@gmail.com>
2021-05-16 21:46:39 +0300

    finished with context API/ no backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import AddTransaction from "./components/AddTransaction";
+import Balance from "./components/Balance";
+import Header from "./components/Header";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import { GlobalProvider } from "./context/GlobalState";
+
 function App() {
   return (
-    <div className="app">
-      <h1>component1</h1>
-      <h2>component2</h2>
-    </div>
+    <GlobalProvider>
+      <Header title="Expense Tracker" />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
