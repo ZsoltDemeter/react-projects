797add2451982f53b3af62db3bcaa1e2ebbfe033
ZsoltDemeter <zsoltd.com@gmail.com>
2021-05-30 14:33:08 +0300

    added form functionality

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,18 +4,128 @@ import contact from "../images/contact.svg";
 import emailIcon from "../images/gmail.png";
 import phoneIcon from "../images/phone.png";
 import React, { useState } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  const [clicked, setClicked] = useState(false);
+  const [hideMessage, setHideMessage] = useState(true);
+  const [errors, setErrors] = useState([]);
 
   const [name, setName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
-  const onClick = () => {
-    setClicked(true);
+  const errorCheck = () => {
+    const errors = validate(name, phoneNumber, email, message);
+
+    if (errors.length > 0) {
+      setErrors(errors);
+      return;
+    } else {
+      setErrors([]);
+    }
   };
+
+  const hide = () => {
+    setHideMessage(true);
+  };
+
+  const SentNotification = () => {
+    return (
+      <div className="message">
+        <span id="X" onClick={hide}>
+          &#10005;
+        </span>
+
+        {errors.length === 0 ? (
+          <h4>Message sent!</h4>
+        ) : (
+          <ul>
+            {errors.map((error) => (
+              <li key={error}>Error: {error}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  };
+
+  function validate(name, phoneNumber, email, message) {
+    const errors = [];
+
+    if (name.length === 0) {
+      errors.push("Name can't be empty");
+    }
+
+    if (phoneNumber.length < 6) {
+      errors.push("Phone number should contain at least 6 characters");
+    }
+
+    if (email.length < 5) {
+      errors.push("Email should be at least 5 charcters long");
+    }
+    if (email.split("").filter((x) => x === "@").length !== 1) {
+      errors.push("Email should contain a @");
+    }
+    if (email.indexOf(".") === -1) {
+      errors.push("Email should contain at least one dot");
+    }
+
+    if (message.length < 8) {
+      errors.push(
+        "Message should not be empty and contain at least 8 characters"
+      );
+    }
+
+    return errors;
+  }
+
+  const onClick = (e) => {
+    e.preventDefault();
+
+    setHideMessage(false);
+
+    errorCheck();
+
+    if (errors.length === 0) {
+      setName("");
+      setPhoneNumber("");
+      setEmail("");
+      setMessage("");
+    }
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setHideMessage(false);
+    errorCheck();
+
+    if (errors.length === 0) {
+      emailjs
+        .sendForm(
+          "service_ys0ytkx",
+          "template_c8iit8p",
+          e.target,
+          "user_Lf9pzgJzJyhgsjWVVB3g5"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+      setName("");
+      setPhoneNumber("");
+      setEmail("");
+      setMessage("");
+
+      e.target.reset();
+      return;
+    }
+  };
+
   return (
     <div className="wrapper" id="contact">
       <img src={contact} alt="Loading.." />
@@ -29,35 +139,40 @@ const Contact = () => {
           <h2>0770480551</h2>
         </div>
       </div>
-      <form className="contact-section">
+      <form className="contact-section" onSubmit={sendEmail}>
         <input
-          className={!name && clicked ? "error" : ""}
+          autoComplete="new-name"
+          name="name"
           type="text"
           placeholder="Name"
           onChange={(e) => setName(e.target.value)}
         />
+
         <input
-          className={!phoneNumber && clicked ? "error" : ""}
+          // autoComplete="new-phone"
+          name="phoneNumber"
           type="text"
           placeholder="Phone Number"
           onChange={(e) => setPhoneNumber(e.target.value)}
         />
         <input
-          className={!email && clicked ? "error" : ""}
+          autoComplete="new-email"
+          name="email"
           type="text"
           placeholder="Email"
           onChange={(e) => setEmail(e.target.value)}
         />
         <textarea
-          className={!message && clicked ? "textarea error" : "textarea"}
+          className="textarea"
+          name="message"
           placeholder="Message me"
           onChange={(e) => setMessage(e.target.value)}
         />
-        <button type="button" onClick={onClick}>
+        <button type="submit" onMouseOver={errorCheck}>
           Send
         </button>
       </form>
-
+      {!hideMessage && <SentNotification />}
       <Footer />
     </div>
   );
